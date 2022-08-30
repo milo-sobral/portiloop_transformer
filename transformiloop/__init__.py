dac25c6093350a8d276b2943cf2e21c19a349074
Milo Sobral <35710239+milo-sobral@users.noreply.github.com>
2022-08-30 14:46:50 -0400

    small fixes for the problems

--- transformiloop/__init__.py
+++ transformiloop/__init__.py
@@ -1,8 +1,8 @@
 import logging
 import os
 
-logging.basicConfig(filename=os.path.join(os.environ['SCRATCH'], 'scripts', 'logs', f"logger_{os.environ['SLURM_ARRAY_JOB_ID']}_{os.environ['$SLURM_ARRAY_TASK_ID']}.out"),
-                    filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S',
-                    level=logging.DEBUG)
+# logging.basicConfig(filename=os.path.join(os.environ['SCRATCH'], 'scripts', 'logs', f"logger_{os.environ['SLURM_ARRAY_JOB_ID']}_{os.environ['$SLURM_ARRAY_TASK_ID']}.out"),
+#                     filemode='a',
+#                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+#                     datefmt='%H:%M:%S',
+#                     level=logging.DEBUG)
