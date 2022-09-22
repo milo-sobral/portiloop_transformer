6c238f2999f630dbfc9340c9957bcd4809c2d45b
Milo Sobral <35710239+milo-sobral@users.noreply.github.com>
2022-09-22 17:39:42 -0400

    changed colab group name

--- tests/test_training.py
+++ tests/test_training.py
@@ -22,4 +22,4 @@ print(config)
 #     depth=3,
 # )
 
-run(config, 'group_test_colab', 'Milo-DEBUG', save_model, unique_name, pretrain, finetune_encoder)
+run(config, 'experiment_32', 'Milo-DEBUG', save_model, unique_name, pretrain, finetune_encoder)
