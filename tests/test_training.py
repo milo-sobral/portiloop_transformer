0f46ecfc5eb210bd24d3f716f04f309f485c8267
Milo Sobral <35710239+milo-sobral@users.noreply.github.com>
2022-09-27 16:10:38 -0400

    last part of fix

--- tests/test_training.py
+++ tests/test_training.py
@@ -22,4 +22,4 @@ print(config)
 #     depth=3,
 # )
 
-run(config, 'experiment_cnn_clip_value', 'Milo-DEBUG', save_model, unique_name, pretrain, finetune_encoder)
+run(config, 'experiment_cnn_stride_fix', 'Milo-DEBUG', save_model, unique_name, pretrain, finetune_encoder)
