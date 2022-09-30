ae4f5934d6ee257e37262121c544605ff112257d
Milo Sobral <35710239+milo-sobral@users.noreply.github.com>
2022-09-30 18:59:57 -0400

    new experiment

--- tests/test_training.py
+++ tests/test_training.py
@@ -22,4 +22,4 @@ print(config)
 #     depth=3,
 # )
 
-run(config, 'experiment_full_transformer', 'Milo-DEBUG', save_model, unique_name, pretrain, finetune_encoder)
+run(config, 'experiment_full_transformer2', 'Milo-DEBUG', save_model, unique_name, pretrain, finetune_encoder)
